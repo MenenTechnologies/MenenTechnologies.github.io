bd258953bc95998f87424d8892b67e053921fdfd
MenenTechnologies <menentechnologies@gmail.com>
2021-02-07 21:47:55 -0500

    Fix: make active the header that matches the current file Renaming resume.html to about.html

--- root/js/include_header.js
+++ root/js/include_header.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $("#navbar").load("navbar.html"); 
+    $("#navbar").load("navbar.html", function () {
+        // load finished:
+        // make active the header that matches the current file
+        $("a.header").each(function (index) {
+            if ( window.location.pathname.indexOf( $(this).attr("id") ) >= 0 ) {
+                $(this).addClass("active");
+            }
+        });
+    });
 });

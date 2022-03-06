9497ef242e822ef542dce1329f40c0889c8bc83d
MenenTechnologies <menentechnologies@gmail.com>
2022-03-06 08:40:52 -0500

    Added tags, descriptions and images. Adjusting layout

--- root/js/load_projects.js
+++ root/js/load_projects.js
@@ -1,26 +1,32 @@
 $.getJSON("root/files/projects.json", function (data) {
-  console.log(data.projects[0]);
+  // console.log(data.projects);
   let projects = data.projects;
 
   function generateCard(project) {
-    div_title = "<h2 class='title'>" + project.title + "</h2>";
-    div_type1 = "<div class='tag type1'>" + project.type1 + "</div>";
-    div_type2 = "<div class='tag type2'>" + project.type2 + "</div>";
-    div_year = "<div class='tag year'>" + project.year + "</div>";
-    button_play = "<div class='play'>Play</div>";
-    image = "<img src='root/images/me/CD1196.jpg'>";
+    div_title = "<h3 class='title'>" + project.title + "</h3>";
+
+    // create elements for tags
+    let tags = [];
+    if (project.tags) {
+      if (project.year) project.tags.push(project.year);
+      for (let i = 0; i < project.tags.length; i++) {
+        tags.push("<div class='tag'>" + project.tags[i] + "</div>");
+      }
+    }
+
+    div_desc = "<div class='description'>" + project.description + "</div>";
+    button_text = project.tags.includes("Game") ? "Play" : "See";
+    button = project.link ? "<a class='button rounded play' href='" + project.link + "'>" + button_text + "</a>" : "";
+    image = "<img src='" + project.image + "'>";
     return (
       "<div class='card'>" +
       "<div class='content'>" +
       div_title +
       "<div class='tags'>" +
-      div_type1 +
-      div_type2 +
-      (project.year != 0 ? div_year : "") +
+      tags.join("") +
       "</div>" +
-      (project.type2 == "Game"
-        ? "<div class='play_container'>" + button_play + "</div>"
-        : "") +
+      div_desc +
+      (button ? "<div class='play_container'>" + button + "</div>" : "") +
       "</div>" +
       "<div class='media'>" +
       image +

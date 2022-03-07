c78c7bff260466baa384f88f06025a722bdec748
MenenTechnologies <menentechnologies@gmail.com>
2022-03-07 18:54:25 -0500

    Adding puldown filters using jQuery UI multiselect

--- root/js/load_projects.js
+++ root/js/load_projects.js
@@ -8,10 +8,10 @@ $.getJSON("root/files/projects.json", function (data) {
     // create elements for tags
     let tags = [];
     if (project.tags) {
-      if (project.year) project.tags.push(project.year);
       for (let i = 0; i < project.tags.length; i++) {
         tags.push("<div class='tag'>" + project.tags[i] + "</div>");
       }
+      if (project.year) tags.push("<div class='tag'>" + project.year + "</div>");
     }
 
     div_desc = "<div class='description'>" + project.description + "</div>";
@@ -19,7 +19,9 @@ $.getJSON("root/files/projects.json", function (data) {
     button = project.link ? "<a class='button rounded play' href='" + project.link + "'>" + button_text + "</a>" : "";
     image = project.image ? "<img src='" + project.image + "'>" : "";
     return (
-      "<div class='card'>" +
+      "<div class='card' id='" +
+      project.tags.join(" ") +
+      "'>" +
       "<div class='content'>" +
       div_title +
       "<div class='tags'>" +
@@ -42,4 +44,37 @@ $.getJSON("root/files/projects.json", function (data) {
     class: "cards",
     html: items.join(""),
   }).appendTo("#projects-container");
+
+  // create filter for tags
+  var tags = new Set();
+  for (let i = 0; i < projects.length; i++) {
+    projects[i].tags.forEach((tag) => tags.add(tag));
+  }
+
+  var groupFilter = $("<div/>", { class: "group-filter" });
+  groupFilter.prependTo("#projects-container");
+
+  $("<div/>", { class: "text", html: "Tags:" }).appendTo(groupFilter);
+  var dropdownTags = $("<select/>", { id: "filter-by-tag", multiple: true });
+  for (var tag of tags) {
+    $("<option />", { value: tag, html: tag }).appendTo(dropdownTags);
+  }
+  dropdownTags.appendTo(groupFilter);
+  $("#filter-by-tag").multiselect({
+    buttonWidth: 150,
+  });
+
+  // create filter for year
+  var years = new Set();
+  projects.forEach((project) => years.add(project.year));
+
+  $("<div/>", { class: "text", html: "Year:" }).appendTo(groupFilter);
+  var dropdownYears = $("<select/>", { id: "filter-by-year", multiple: true });
+  for (let year = 2019; year < 2022; year++) {
+    $("<option />", { value: year, html: year }).appendTo(dropdownYears);
+  }
+  dropdownYears.appendTo(groupFilter);
+  $("#filter-by-year").multiselect({
+    buttonWidth: 150,
+  });
 });

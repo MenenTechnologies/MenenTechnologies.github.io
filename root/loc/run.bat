0a2b820ece2f4abccd1887987a5d6b0416c26b75
MenenTechnologies <menentechnologies@gmail.com>
2022-03-15 17:30:09 -0400

    Adding HTML files to localization

--- root/loc/run.bat
+++ root/loc/run.bat
@@ -1,3 +1,15 @@
-serge pull-ts config.nconf
-serge localize config.nconf
-serge push-ts config.nconf
+>run_log.txt (
+	echo PULLING TRANSLATIONS
+	call serge pull-ts config.nconf
+	
+	echo TRANSLATING
+	call serge localize config.nconf
+	
+	echo PUSHING TRANSLATIONS
+	call serge push-ts config.nconf
+	
+	echo:
+	echo COPYING FILES
+	copy /y ..\..\navbar.html ..\..\es\navbar.html
+	copy /y ..\..\projects.html ..\..\es\projects.html
+)
